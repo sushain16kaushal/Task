4723aca59a3904776bc69063b4d3194b65d11eae
Sushain Kaushal <sushainkaushal@gmail.com>
2026-06-25 03:34:42 +0530

    server-cors update

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ import { configDotenv } from 'dotenv';
 configDotenv();
 const app = express();
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://task-eta-snowy-99.vercel.app/",
     credentials:true
 }));
 mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@clusterproduct.vvfbzyh.mongodb.net/`)
